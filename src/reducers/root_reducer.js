3f046198cfbe96609124d334c90aad15527d9cbe
Scott Snyder <ssny88@gmail.com>
2017-05-10 14:34:25 -0700

    add product to reducer

--- src/reducers/root_reducer.js
+++ src/reducers/root_reducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 
 import ProductsReducer from './products_reducer';
+import productRecuder from './products_reducer';
 
 const rootReducer = combineReducers({
-  products: ProductsReducer
+  products: ProductsReducer,
+  product: productRecuder
 });
 
 export default rootReducer;
