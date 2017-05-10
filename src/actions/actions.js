68c7a6e46be4df06993973f60c0c11cf3fd4da0e
Scott Snyder <ssny88@gmail.com>
2017-05-10 10:41:34 -0700

    add product reducer actions

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,12 +1,21 @@
 import Axios from 'axios';
 
-import {FETCH_PRODUCTS} from './types';
+import { FETCH_PRODUCTS, FETCH_PRODUCT } from './types';
 
 export function fetchProducts(){
-  let request = Axios.get('/products')
+  let request = Axios.get('/products');
 
   return {
     type: FETCH_PRODUCTS,
+    payload: request
+  }
+};
+
+export function fetchProduct(){
+  let request = Axios.get('/products/1');
+
+  return {
+    type: FETCH_PRODUCT,
     payload: request
   }
 };
--- src/actions/types.js
+++ src/actions/types.js
@@ -1,1 +1,2 @@
 export const FETCH_PRODUCTS = "FETCH_PRODUCTS";
+export const FETCH_PRODUCT = "FETCH_PRODUCT";
