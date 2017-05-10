a202c7bf92b4c37e5492700a84998ae1ffa310b2
Scott Snyder <ssny88@gmail.com>
2017-05-10 10:19:05 -0700

    create components for central page elements

---
+++ src/components/product_item.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchProduct } from '../actions/actions';
+
+class ProductItem extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    this.props.fetchProduct();
+  }
+
+  render() {
+    <div>
+      <h2>{product.name}</h2>
+      <ul>
+        <li>{product.description}</li>
+        <li>{product.numberRemaining}</li>
+        <li>{product.categories}</li>
+        <li>{product.price}</li>
+      </ul>
+    </div>
+  }
+}
+
+const mapStateToProps = ({ product }) => (
+  { product }
+);
+
+export default connect(mapStateToProps, {fetchProduct})(ProductItem);
