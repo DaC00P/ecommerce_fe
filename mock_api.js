caa679bbc7b682928e3678f155d4b62f7d1e552c
Scott Snyder <ssny88@gmail.com>
2017-05-10 14:30:00 -0700

    add IDs to mock data

--- mock_api.js
+++ mock_api.js
@@ -19,10 +19,10 @@ Server.use(BodyParser.json({
   limit: 1024 * 1024 * 10
 }));
 
-const mockProducts = {products: [{name: 'ball', description: 'big red ball', numberRemaining: 2, categories: ['sports', 'balls', 'bouncy toys'], price: 5},
-                                  {name: 'chew bone', description: 'doggie chew bone', numberRemaining: 5, categories: ['pets', 'pet toys'], price: 3},
-                                  {name: 'doll', description: 'a cool hipster doll wearing plaid', numberRemaining: 1, categories: ['dolls'], price: 5},
-                                  {name: 'ferrari', description: 'because adults need toys too', numberRemaining: 11, categories: ['adult toys', 'cars', 'luxery'], price: 100000}]}
+const mockProducts = {products: [{id: 2, name: 'ball', description: 'big red ball', numberRemaining: 2, categories: ['sports', 'balls', 'bouncy toys'], price: 5},
+                                  {id: 3, name: 'chew bone', description: 'doggie chew bone', numberRemaining: 5, categories: ['pets', 'pet toys'], price: 3},
+                                  {id: 4, name: 'doll', description: 'a cool hipster doll wearing plaid', numberRemaining: 1, categories: ['dolls'], price: 5},
+                                  {id: 1, name: 'ferrari', description: 'because adults need toys too', numberRemaining: 11, categories: ['adult toys', 'cars', 'luxery'], price: 100000}]}
 
 const mockSingleProduct = {id: 1, name: 'ferrari', description: 'because adults need toys too', numberRemaining: 11, categories: ['adult toys', 'cars', 'luxery'], price: 100000}
 
