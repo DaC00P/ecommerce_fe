7b6d6eb029da5df0081d800a96d07da94da4a301
CJButton <gsyme1907@gmail.com>
2017-05-14 21:50:16 -0700

    basics of auth setup

---
+++ ecommerce_fe/config/routes.rb
@@ -0,0 +1,10 @@
+
+
+Rails.application.routes.draw do
+  # devise_for :users
+  devise_for :user, only: []
+
+ namespace :v1, defaults: { format: :json } do
+   resource :login, only: [:create], controller: :sessions
+ end
+end
