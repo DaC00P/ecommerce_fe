f15547a20c790707240035597b8d67412581e8ca
Scott Snyder <ssny88@gmail.com>
2017-05-16 10:42:55 -0700

    render shopping cart content on main page - for testing

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import ProductList from './product_list';
+import ShoppingCart from './shopping_cart';
 
 export default class App extends Component {
   render() {
@@ -8,6 +9,7 @@ export default class App extends Component {
       <div>
         WELCOME TO CRAIGSLIST-REDUX Y'ALL!
         <ProductList />
+        <ShoppingCart />
       </div>
     );
   }
