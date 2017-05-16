4dfe892e2f4f5ddf3e008a331f800abf0ffbe6f4
Scott Snyder <ssny88@gmail.com>
2017-05-16 10:42:11 -0700

    run react DOM render after page loads

--- src/ecommerce.js
+++ src/ecommerce.js
@@ -1,23 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { Router, browserHistory } from 'react-router';
 import rootReducer from './reducers/root_reducer';
 import promise from 'redux-promise';
-
-import App from './components/app'
+import Root from './root';
 
 const createStoreWithMiddleware = applyMiddleware(
   promise
 )(createStore);
 const store = createStoreWithMiddleware(rootReducer);
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+document.addEventListener("DOMContentLoaded", () => {
+  ReactDOM.render(<Root store={store} />, document.querySelector('.container'));
+});
 
 
 window.store = store;
